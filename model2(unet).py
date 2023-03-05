c722fa54b2beb47ab21b3d062a0732e25ff6e122
WhiteWolf47 <bhandari2003anurag@gmail.com>
2023-03-05 05:51:31 +0000

    initial commit

---
+++ model2(unet).py
@@ -0,0 +1,321 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import tensorflow as tf
+import os
+from tqdm import tqdm
+import warnings
+import seaborn as sns
+'''import mlflow
+from mlflow.models.signature import ModelSignature
+from mlflow.types.schema import Schema, TensorSpec'''
+
+#schema for the model input and output where the input is a 512x256x3 image and the output is a 512x256x3 image
+'''SIGNATURE = ModelSignature(
+    inputs=Schema(
+        [
+            TensorSpec(
+                np.dtype(np.float32), [None, 512, 256, 3], name="input"
+            )
+        ]
+    ),
+    outputs=Schema(
+        [
+            TensorSpec(
+                np.dtype(np.float32), [None, 512, 256, 1], name="output"
+            )
+        ]
+    )
+)'''
+
+#track experiment
+#mlflow.set_tracking_uri("http://localhost:5000")
+
+warnings.filterwarnings("ignore")
+
+plt.style.use("seaborn-darkgrid")
+
+sns.set_context("paper", font_scale=1.4)
+
+BATCH_SIZE = 32
+
+id_map = {
+    0: (0, 0, 0), # unlabelled
+    1: (111, 74,  0), #static
+    2: ( 81,  0, 81), #ground
+    3: (128, 64,127), #road
+    4: (244, 35,232), #sidewalk
+    5: (250,170,160), #parking
+    6: (230,150,140), #rail track
+    7: (70, 70, 70), #building
+    8: (102,102,156), #wall
+    9: (190,153,153), #fence
+    10: (180,165,180), #guard rail
+    11: (150,100,100), #bridge
+    12: (150,120, 90), #tunnel
+    13: (153,153,153), #pole
+    14: (153,153,153), #polegroup
+    15: (250,170, 30), #traffic light
+    16: (220,220,  0), #traffic sign
+    17: (107,142, 35), #vegetation
+    18: (152,251,152), #terrain
+    19: ( 70,130,180), #sky
+    20: (220, 20, 60), #person
+    21: (255,  0,  0), #rider
+    22: (  0,  0,142), #car
+    23: (  0,  0, 70), #truck
+    24: (  0, 60,100), #bus
+    25: (  0,  0, 90), #caravan
+    26: (  0,  0,110), #trailer
+    27: (  0, 80,100), #train
+    28: (  0,  0,230), #motorcycle
+    29: (119, 11, 32), #bicycle
+    30: (  0,  0,142) #license plate 
+}
+
+category_map = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 1,
+    7: 2,
+    8: 2,
+    9: 2,
+    10: 2,
+    11: 2,
+    12: 2,
+    13: 3,
+    14: 3,
+    15: 3,
+    16: 3,
+    17: 4,
+    18: 4,
+    19: 5,
+    20: 6,
+    21: 6,
+    22: 7,
+    23: 7,
+    24: 7,
+    25: 7,
+    26: 7,
+    27: 7,
+    28: 7,
+    29: 7,
+    30: 7
+}
+
+num_classes = len(id_map.keys())
+
+def preprocess(path):
+    img = Image.open(path)
+    img1 = img.crop((0, 0, 256, 256)).resize((128, 128))
+    img2 = img.crop((256, 0, 512, 256)).resize((128, 128))
+    img1 = np.array(img1) / 255.
+    img2 = np.array(img2)
+    mask = np.zeros(shape=(img2.shape[0], img2.shape[1]), dtype = np.uint32)
+    for row in range(img2.shape[0]):
+        for col in range(img2.shape[1]):
+            a = img2[row, col, :]
+            final_key = None
+            final_d = None
+            for key, value in id_map.items():
+                d = np.sum(np.sqrt(pow(a - value, 2)))
+                if final_key == None:
+                    final_d = d
+                    final_key = key
+                elif d < final_d:
+                    final_d = d
+                    final_key = key
+            mask[row, col] = final_key
+    mask = np.reshape(mask, (mask.shape[0], mask.shape[1], 1))
+    del img2
+    return img1, mask
+
+def prepare_tensor_dataset(train_path, val_path):
+    X_train = []
+    Y_train = []
+    X_val = []
+    Y_val = []
+    for file in tqdm(os.listdir(train_path)):
+        img, mask = preprocess(f"{train_path}/{file}")
+        X_train.append(img)
+        Y_train.append(mask)
+    
+    for file in tqdm(os.listdir(val_path)):
+        img, mask = preprocess(f"{val_path}/{file}")
+        X_val.append(img)
+        Y_val.append(mask)
+
+    return X_train, Y_train, X_val, Y_val
+
+X_train, Y_train, X_valid, Y_valid = prepare_tensor_dataset("../input/cityscapes-image-pairs/cityscapes_data/train/", "../input/cityscapes-image-pairs/cityscapes_data/val/")
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+X_valid = np.array(X_valid)
+Y_valid = np.array(Y_valid)
+
+def get_unet_model():
+    
+    inputs = tf.keras.layers.Input(shape = [128, 128, 3])
+    
+    #First Downsample
+    f1 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(inputs)
+    b1 = tf.keras.layers.BatchNormalization()(f1)
+    f2 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b1)    # Used later for residual connection
+    
+    m3 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f2)
+    d4 = tf.keras.layers.Dropout(0.2)(m3)
+    
+    # Second Downsample
+    f5 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d4)
+    b5 = tf.keras.layers.BatchNormalization()(f5)
+    f6 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b5)    # Used later for residual connection
+    
+    m7 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f6)
+    d8 = tf.keras.layers.Dropout(0.2)(m7)
+    
+    # Third Downsample
+    f9 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d8)
+    b9 = tf.keras.layers.BatchNormalization()(f9)
+    f10 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b9)    # Used later for residual connection
+    
+    m11 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f10)
+    d12 = tf.keras.layers.Dropout(0.2)(m11)
+    
+    #Forth Downsample
+    f13 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d12)
+    b13 = tf.keras.layers.BatchNormalization()(f13)
+    f14 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b13)    # Used later for residual connection
+    
+    m15 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f14)
+    d16 = tf.keras.layers.Dropout(0.2)(m15)
+    
+    #Fifth Downsample
+    f17 = tf.keras.layers.Conv2D(1024, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d16)
+    b17 = tf.keras.layers.BatchNormalization()(f17)
+    f18 = tf.keras.layers.Conv2D(1024, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b17)
+
+    
+    # First Upsample
+    m19 = tf.keras.layers.UpSampling2D(size = (2, 2))(f18)
+    d19 = tf.keras.layers.Dropout(0.2)(m19)
+    c20 = tf.keras.layers.Concatenate()([d19, f14])
+    f21 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1 ,activation = "relu")(c20)
+    b21 = tf.keras.layers.BatchNormalization()(f21)
+    f22 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b21)
+    
+    # Second Upsample
+    m23 = tf.keras.layers.UpSampling2D(size = (2, 2))(f22)
+    d23 = tf.keras.layers.Dropout(0.2)(m23)
+    c24 = tf.keras.layers.Concatenate()([d23, f10])
+    f25 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(c24)
+    b25 = tf.keras.layers.BatchNormalization()(f25)
+    f26 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b25)
+    
+    # Third Upsample
+    m27 = tf.keras.layers.UpSampling2D(size = (2, 2))(f26)
+    d27 = tf.keras.layers.Dropout(0.2)(m27)
+    c28 = tf.keras.layers.Concatenate()([d27, f6])
+    f29 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(c28)
+    b29 = tf.keras.layers.BatchNormalization()(f29)
+    f30 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b29)
+    
+    #Forth Upsample
+    m31 = tf.keras.layers.UpSampling2D(size = (2, 2))(f30)
+    d31 = tf.keras.layers.Dropout(0.2)(m31)
+    c32 = tf.keras.layers.Concatenate()([d31, f2])
+    f33 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(c32)
+    b33 = tf.keras.layers.BatchNormalization()(f33)
+    f34 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b33)
+    
+    # Output Layer
+    outputs = tf.keras.layers.Conv2D(num_classes, kernel_size = (3, 3), padding = "same", strides = 1, activation = "softmax")(f34)
+    
+    model = tf.keras.Model(inputs = [inputs], outputs = [outputs])
+    return model
+
+model = get_unet_model()
+
+#tf.keras.utils.plot_model(model, show_shapes = True)
+
+class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
+  def __init__(self,
+               y_true=None,
+               y_pred=None,
+               num_classes=None,
+               name=None,
+               dtype=None):
+    super(UpdatedMeanIoU, self).__init__(num_classes = num_classes,name=name, dtype=dtype)
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    y_pred = tf.math.argmax(y_pred, axis=-1)
+    return super().update_state(y_true, y_pred, sample_weight)
+
+class VizCallback(tf.keras.callbacks.Callback):
+    
+    def __init__(self, file_path, **kwargs):
+        super().__init__(**kwargs)
+        self.file_path = file_path
+    
+    def on_epoch_end(self, epoch, logs=None):
+        img, mask = preprocess(self.file_path)
+        img = np.array(img)
+        img = np.reshape(img, (1, 128, 128, 3))
+        pred = model.predict(img)
+        y_pred = tf.math.argmax(pred, axis=-1)
+        y_pred = np.array(y_pred)
+        y_pred = np.reshape(y_pred, (128, 128))
+        fig, axes = plt.subplots(nrows = 1, ncols = 2)
+        axes[0].imshow(mask)
+        axes[0].set_title("Original Mask")
+        axes[1].imshow(y_pred)
+        axes[1].set_title("Predicted Mask")
+        plt.tight_layout()
+        plt.show()
+
+def plot_history(history):
+  fig, axes = plt.subplots(nrows = 1, ncols = 3, figsize=(20, 7))
+  # Training
+  sns.lineplot(range(1, len(history.history["loss"]) + 1), history.history["loss"], ax = axes[0], label="Training Loss")
+  sns.lineplot(range(1, len(history.history["loss"]) + 1), history.history["accuracy"], ax = axes[1], label="Training Accuracy")
+  sns.lineplot(range(1, len(history.history["loss"]) + 1), history.history["mean_iou"], ax = axes[2], label="Training Mean IOU")
+
+  # Validation
+  sns.lineplot(range(1, len(history.history["loss"]) + 1), history.history["val_loss"], ax = axes[0], label="Validation Loss")
+  sns.lineplot(range(1, len(history.history["loss"]) + 1), history.history["val_accuracy"], ax = axes[1], label="Validation Accuracy")
+  sns.lineplot(range(1, len(history.history["loss"]) + 1), history.history["val_mean_iou"], ax = axes[2], label="Validation Mean IOU")
+  
+  axes[0].set_title("Loss Comparison", fontdict = {'fontsize': 15})
+  axes[0].set_xlabel("Epoch")
+  axes[0].set_ylabel("Loss")
+
+  axes[1].set_title("Accuracy Comparison", fontdict = {'fontsize': 15})
+  axes[1].set_xlabel("Epoch")
+  axes[1].set_ylabel("Accuracy")
+
+  axes[2].set_title("Mean IOU Comparison", fontdict = {'fontsize': 15})
+  axes[2].set_xlabel("Epoch")
+  axes[2].set_ylabel("Mean IOU")
+  plt.tight_layout()
+  plt.show()
+
+model.compile(loss = "sparse_categorical_crossentropy", optimizer = "adam", metrics = ["accuracy", UpdatedMeanIoU(num_classes=num_classes, name = "mean_iou")])
+
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience = 10, restore_best_weights = True)
+viz_callback = VizCallback("./val/106.jpg")
+
+history = model.fit(x=X_train, y=Y_train, epochs = 100, batch_size = BATCH_SIZE, validation_data = (X_valid, Y_valid), callbacks=[early_stopping, viz_callback])
+
+#logging the model
+#mlflow.tensorflow.log_model(model, "model", signature=SIGNATURE)
+
+#end the experiment
+#mlflow.end_run()
+
+
+plot_history(history)
---
+++ task1_app.py
@@ -0,0 +1,165 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="Semantic Segmentation", page_icon=":smile:", layout="wide", initial_sidebar_state="expanded")
+# ----------- General things
+valid_molecule = True
+loaded_molecule = None
+selection = None
+submit = None
+
+lm1 = ["Unet_Model1", "Unet_Model2", "ResNet-38", "ResNet-101"]
+lm2 = [0.9111, 0.7866, 0.7840, 0.7760]
+lm3 = [0.3124, 0.7456, " ", " "]
+lm4 = ["Continuous Learning", "Continuous Learning", "Batch Learning", "Batch Learning"]
+lm5 = ["[GitHub](https://github.com/WhiteWolf47/lfx_task1)", "[GitHub](https://github.com/WhiteWolf47/lfx_task1)", "https://github.com/itijyou/ademxapp", "https://github.com/TuSimple/TuSimple-DUC"]
+
+#Dataframe for the table
+df_bm = pd.DataFrame(list(zip(lm1, lm2, lm3, lm4, lm5)), columns =['Model', 'Accuracy', 'Loss', 'Learning Paradigm', 'Github Link'])
+
+# ----------- Sidebar
+page = st.sidebar.selectbox('Page Navigation', ["Home Page", "Documentation", "Download", "Benchmarking"])
+
+st.sidebar.markdown("""---""")
+st.sidebar.write("[Github](https://github.com/WhiteWolf47/cscapes_semantic_segmentation)")
+st.sidebar.title("Dataset Sample")
+
+if page == "Home Page":
+
+    # ----------- Inputs
+    st.title("Dataset Overview")
+    st.write("Cityscapes is a large-scale database which focuses on semantic understanding of urban street scenes. It provides semantic, instance-wise, and dense pixel annotations for 30 classes grouped into 8 categories (flat surfaces, humans, vehicles, constructions, objects, nature, sky, and void). The dataset consists of around 5000 fine annotated images and 20000 coarse annotated ones. Data was captured in 50 cities during several months, daytimes, and good weather conditions. It was originally recorded as video so the frames were manually selected to have the following features: large number of dynamic objects, varying scene layout, and varying background.")
+    
+    st.image(["img3.png", "img4.png", "img2.png"], width=470)
+
+    st.title("Lifelong learning algorithm overview")
+    st.write("Lifelong Machine Learning or Lifelong Learning (LL) is an advanced machine learning (ML) paradigm that learns continuously, accumulates the knowledge learned in the past, and uses/adapts it to help future learning and problem solving. In the process, the learner becomes more and more knowledgeable and better and better at learning. This continuous learning ability is one of the hallmarks of human intelligence.")
+    
+    st.header("Some of the LL algorithms are:")
+    
+    st.subheader("1. MTL net (Multi-task learning with neural network) Caruana")
+    st.write("Although MTL net (Multi-task learning with neural network) Caruana is described as a lifelong learning method in , it is actually a batch multi-task learning method. Based on our definition of lifelong learning, they are different learning paradigms In MTL net, instead of building a neural network for each individual task, it constructs a universal neural network for all the tasks. This universal neural network uses the same input layer for input from all tasks and uses one output unit for each task (or class in this case). There is also a shared hidden layer in MTL net that is trained in parallel using Back-Propagation on all the tasks to minimize the error on all the tasks. This shared layer allows features developed for one task to be used by other tasks.")
+    
+    st.subheader("2. LIFELONG EBNN")
+    st.write("This lifelong learning approach is in the context of EBNN (Explanation-Based Neural Network), which again leverages the previous task data (or the support set) to improve learning. Concept learning is the goal of this work, which learns a function f : I → {0, 1} to predict if an object represented by a feature vector x ∈ I belongs to a concept (y = 1) or not (y = 0). In this approach, the system first learns a general distance function, d : I × I → [0, 1], considering all the past data (or the support set) and uses this distance function to share or transfer the knowledge of the past task data to the new task TN+1. Given two input vectors, say x and x' , function d computes the probability of x and x0 being members of the same concept (or class), regardless what the concept is.")
+    
+    st.subheader("3. LTM: A LIFELONG TOPIC MODEL")
+    st.write("Topic modeling has been commonly used to discover topics from document collections. However, unsupervised models can generate many incoherent topics. To address this problem, several knowledge-based topic models have been proposed to incorporate prior domain knowledge from the user. This work advances this research much further and shows that without any user input, we can mine the prior knowledge automatically and dynamically from topics already found from a large number of domains. This paper first proposes a novel method to mine such prior knowledge dynamically in the modeling process, and then a new topic model to use the knowledge to guide the model inference. What is also interesting is that this approach offers a novel lifelong learning algorithm for topic discovery, which exploits the big (past) data and knowledge gained from such data for subsequent modeling. Our experimental results using product reviews from 50 domains demonstrate the effectiveness of the proposed approach.")
+    st.write("For a detailed overview of the above algorithms, please refer to this [document](https://www.cs.uic.edu/~liub/lifelong-machine-learning-draft.pdf)")
+
+    #Data Sample Display
+    st.title("Dataset Sample")
+    st.write("The dataset consists of around 5000 fine annotated images and 20000 coarse annotated ones. Data was captured in 50 cities during several months, daytimes, and good weather conditions. It was originally recorded as video so the frames were manually selected to have the following features: large number of dynamic objects, varying scene layout, and varying background.")
+    st.header("Training Sample:")
+    st.image(["1.jpg", "2.jpg", "3.jpg"], width=470)
+    st.header("Validation Sample:")
+    st.image(["val/4.jpg", "val/5.jpg", "val/6.jpg"], width=470)
+    st.header("Test Sample:")
+    st.image(["test/1test.png", "test/2test.png", "test/3test.png"], width=470)
+
+elif page == "Documentation":
+    st.title("Dataset partition description")
+    l1 = ["flat", "human", "vehicle", "construction", "object", "nature", "sky", "void"]
+    l2 = ["road · sidewalk · parking+ · rail track+", "person* · rider*", "car* · truck* · bus* · on rails* · motorcycle* · bicycle* · caravan*+ · trailer*+", "building · wall · fence · guard rail+ · bridge+ · tunnel+", "pole · pole group+ · traffic sign · traffic light", "vegetation · terrain", "sky", "ground+ · dynamic+ · static+"]
+    df = pd.DataFrame(list(zip(l1, l2)), columns =['Group', 'Classes'])
+    df.reset_index(drop=True, inplace=True)
+    st.table(df)
+
+    st.title("Data statistics")
+    st.header("Features")
+    st.subheader("Polygonal annotations")
+    st.write("Dense semantic segmentation")
+    st.write("Instance segmentation for vehicle and people")
+
+    st.subheader("Complexity")
+    st.write("30 classes")
+    st.write("See Class Definitions for a list of all classes and have a look at the applied labeling policy.")
+
+    st.subheader("Diversity")
+    st.write("50 cities")
+    st.write("Several months (spring, summer, fall)")
+    st.write("Daytime")
+    st.write("Good/medium weather conditions")
+    st.write("5000 annotated images with fine annotations (examples)")
+    st.write("20000 annotated images with coarse annotations (examples)")
+    st.subheader("MetaData")
+    st.write("Preceding and trailing video frames. Each annotated image is the 20th image from a 30 frame video snippets (1.8s)")
+    st.write("Corresponding right stereo views")
+    st.write("Good/medium weather conditions")
+    st.write("Ego-motion data from vehicle odometry")
+    st.write("Outside temperature from vehicle sensor")
+    st.write("GPS coordinates")
+    st.subheader("Extensions by other researchers")
+    st.write("Bounding box annotations of people")
+    st.write("Images augmented with fog and rain")
+    st.subheader("Benchmark suite and evaluation server")
+    st.write("Pixel-level semantic labeling")
+    st.write("Instance-level semantic labeling")
+    st.write("Panoptic semantic labeling")
+
+    st.title("Data Format")
+    st.write("The data is provided in the form of images and annotations. The images are provided in the form of png files. The annotations are provided in the form of json files. The json files contain the following fields:")
+    st.write("imagePath: The path to the image file.")
+    st.write("imageData: The image data in base64 encoding.")
+    st.write("imageHeight: The height of the image.")
+    st.write("imageWidth: The width of the image.")
+    st.write("imageId: The id of the image.")
+    st.write("annotations: The annotations of the image.")
+    st.write("The annotations field contains a list of annotations. Each annotation contains the following fields:")
+    st.write("segmentation: The segmentation mask of the object.")
+    st.write("area: The area of the object.")
+    st.write("iscrowd: Whether the object is a crowd.")
+    st.write("imageId: The id of the image.")
+    st.write("bbox: The bounding box of the object.")
+    st.write("category_id: The id of the category.")
+    st.write("id: The id of the annotation.")
+    st.write("categories: The categories of the dataset.")
+    st.write("The categories field contains a list of categories. Each category contains the following fields:")
+    st.write("supercategory: The supercategory of the category.")
+    st.write("id: The id of the category.")
+    st.write("name: The name of the category.")
+    st.write("Example: /cityscapes/train/leftImg8bit/cityscapes_real/aachen/aachen_000000_000019_leftImg8bit.png")
+
+    st.title("Annotations")
+    st.header("Fine annotations")
+    st.write("Below are examples of our high quality dense pixel annotations that we provide for a volume of 5000 images. Overlayed colors encode semantic classes (see class definitions). Note that single instances of traffic participants are annotated individually.")
+    st.image(["fan1.png", "fan2.png", "fan3.png"], width=470)
+
+    st.header("Coarse annotations")
+    st.write("In addition to the fine annotations, we provide coarser polygonal annotations for a set of 20 000 images in collaboration with Pallas Ludens. Again, overlayed colors encode the semantic classes (see class definitions). Note that we do not aim to annotated single instances, however, we marked polygons covering individual objects as such.")
+    st.image(["can1.png", "can2.png", "can3.png"], width=470)
+
+    st.header("Videos")
+    st.write("The videos below provide further examples of the Cityscapes Dataset. The first video contains roughly 1000 images with high quality annotations overlayed. The second video visualizes the precomputed depth maps using the corresponding right stereo views. The last video is extracted from a long video recording and visualizes the GPS positions as part of the dataset’s metadata. Note that the images are blurred for privacy reasons.")
+    
+    v1 = open('v1.mp4', 'rb')
+    v1b = v1.read()
+    st.video(v1b)
+
+elif page == "Download":
+
+    st.title("Instructions")
+    st.write("For downloading the data, you need to register on the cityscapes website. After registration, you will receive an email with a link to activate your account. After activation, you can log in and download the data. The data is available in the form of images and annotations. The images are provided in the form of png files. The annotations are provided in the form of json files. The json files contain the following fields:")
+
+    st.title("Download")
+    st.write("The dataset is available for download at the following link: https://www.cityscapes-dataset.com/downloads/")
+
+else:
+    #Benchmarking
+    st.title("Benchmarking")
+
+    st.subheader("The following graph shows the benchmarking results for the different models:")
+    df1 = df_bm[::-1]
+    fig = px.line(df1, x="Model", y="Accuracy")
+    st.plotly_chart(fig)
+
+
+    st.subheader("The following table shows the benchmarking results for the different models:")
+    st.table(df_bm)
+
+
+
+
+
+
